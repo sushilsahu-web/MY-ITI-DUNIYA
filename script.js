00822c57f4216880194145443fc1715dae1a3a0a
sushilsahu-web <sahusushil694@gmail.com>
2025-06-02 23:39:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,11 @@ function showClassmate(id) {
 }
 
 // Image Slider
-const sliderImages = ["slider1.jpg", "slider2.jpg", "slider3.jpg", "slider4.jpg", "slider5.jpg"];
+const sliderImages = [
+  "kaj1.jpg", "kaj2.webp", "kaj3.jpg", "kaj4.jpg", "kaj5.jpg",
+  "kaj6.jpg", "kaj7.jpg", "kaj8.webp", "kaj9.webp", "kaj10.webp",
+  "kaj11.webp", "kaj12.webp", "kaj13.jpg", "kaj14.webp", "kaj15.jpg"
+];
 let currentSlide = 0;
 
 function showSlide(index) {
@@ -52,3 +56,7 @@ function nextSlide() {
 function prevSlide() {
   showSlide(currentSlide - 1);
 }
+
+window.onload = function() {
+  showSlide(currentSlide); // First image show hote hi
+};
